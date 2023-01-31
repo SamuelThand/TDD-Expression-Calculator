e1ef8941a294fec843088ee70087d2aa645045a2
Samuel Thand <sath2102@student.miun.se>
2023-01-31 16:40:19 +0100

    Argparsing and Calculator class
    Implemented argparsing and built the Calculator class with the non
    implemented calculate public method.

--- dt042g_src/calculator.py
+++ dt042g_src/calculator.py
@@ -2,7 +2,27 @@
 
 import argparse
 import json
+import sys
+
+__version__ = '1.0'
+__desc__ = "A simple calculator that evaluates a mathematical expression using + - / * ()"
+
+
+class Calculator:
+
+    def calculate(self, calculation) -> str:
+        pass
+
+
+def main():
+    epilog = 'DT042G Calculator V' + __version__
+    parser = argparse.ArgumentParser(description=__desc__, epilog=epilog, add_help=True)
+    parser.add_argument('-c', '--calculate', dest='calculate', type=str,
+                        help='The expression to calculate. Ex: 2+(4-5)/5*3')
+    arguments = parser.parse_args()
+
+    print(Calculator().calculate(arguments.calculate))
 
 
 if __name__ == '__main__':
-    pass
+    main()
